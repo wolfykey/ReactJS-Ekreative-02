f816dd1f8bdf92f8d6e6f4d611093ca8a671cdad
Vlad Vovk <wolfykey@gmail.com>
2019-12-06 23:05:55 +0200

    update task-02

---
+++ task-02/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import FormRegistration from '../FormRegistration/FormRegistration'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Hello! Lets create your account!</p>
+        <FormRegistration />
+      </header>
+    </div>
+  )
+}
+
+export default App
